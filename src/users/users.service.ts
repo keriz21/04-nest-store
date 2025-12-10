71d6965d55153167f46a29f1b20a6ae62483f4c3
Reza Ali Nirwansyah <rezaalinirwansyah@gmail.com>
2025-12-10 09:44:19 +0700

    init app with nestjs and typeorm sqlite

---
+++ src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  create(name: string, email: string, password: string) {
+    const user = this.userRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    this.userRepository.save(user);
+    return user;
+  }
+}
